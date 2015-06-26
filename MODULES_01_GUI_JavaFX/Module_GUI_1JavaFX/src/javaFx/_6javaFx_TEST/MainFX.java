1205bbe92703927f72a61a314ccc574be9388b51
atafs <atafs@iscte-iul.pt>
2015-06-26 22:23:32 +0100

    web browser in java fx

---
+++ MODULES_01_GUI_JavaFX/Module_GUI_1JavaFX/src/javaFx/_6javaFx_TEST/MainFX.java
@@ -0,0 +1,49 @@
+package javaFx._6javaFx_TEST;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainFX extends Application{
+	
+	//ATRIBUTES
+	private Label lb_text;
+	private Button btn_click;
+	
+	//MAIN
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	//START
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		//NEW ELEMENTS
+		lb_text = new Label("Here some Text");
+		btn_click = new Button("Click it");
+		
+		//LISTENER
+		btn_click.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				lb_text.setText("New Text");				
+			}
+		});
+		
+		//INITIALIZE WINDOW
+		VBox root = new VBox();
+		root.getChildren().addAll(lb_text, btn_click);
+		
+		Scene scene = new Scene(root, 500,500);
+		stage.setScene(scene);
+		
+		stage.show();
+	}
+}
